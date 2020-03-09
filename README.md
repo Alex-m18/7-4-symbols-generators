e44d696bcfa2698cd7173759b7611c987b5d60cd
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-03-09 16:12:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/g1nwbiba829apf50?svg=true)](https://ci.appveyor.com/project/Alex-m18/7-4-symbols-generators)
+# 7-4-symbols-generators
---
+++ src/js/Team.js
@@ -0,0 +1,45 @@
+/**
+ * Team class. Includes members set without doubles
+ * @class
+ */
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  /**
+   * Add new unique character in the team members set
+   * @param {Character} character
+   * @throws if character is in the members set already
+   */
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Character is in the members set already');
+    }
+    this.members.add(character);
+  }
+
+  /**
+   * Add some new unique characters in the team members set
+   * @param  {...Character} rest
+   */
+  addAll(...rest) {
+    for (const character of rest) this.members.add(character);
+  }
+
+  /**
+   * Returns team members as array
+   */
+  toArray() {
+    return [...this.members];
+  }
+
+  * [Symbol.iterator]() {
+    for (const member of this.members) {
+      yield member;
+    }
+    return 0;
+  }
+}
+
+export default Team;
